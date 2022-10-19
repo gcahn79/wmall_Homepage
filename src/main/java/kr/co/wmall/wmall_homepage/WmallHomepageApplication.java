9dd92b5103118eaca939cae75dbac588036d2811
GeunChang Ahn <13996827+rkaehdaos@users.noreply.github.com>
2022-10-19 13:58:36 +0900

    init project

---
+++ src/main/java/kr/co/wmall/wmall_homepage/WmallHomepageApplication.java
@@ -0,0 +1,13 @@
+package kr.co.wmall.wmall_homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmallHomepageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmallHomepageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/wmall/wmall_homepage/WmallHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.wmall.wmall_homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmallHomepageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
