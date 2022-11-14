518cb7dc0ced907f7a05eb7abd51a7feec30bfd5
안근창 차장 <gcahn79@w-mall.co.kr>
2022-11-14 14:41:15 +0900

    Web mvc security 1차 #5 (#13)
    * [#5 webMvc and Security] - `@EnableWebSecurity`
    
    - Spring Configuration
    - 쉽게 MVC Security세팅을 할 수 있게 도와준다라는데..
    
    * [#5 webMvc and Security] PasswordEncoder 빈 정의 - BCryptPasswordEncoder
    
    - BCryptPasswordEncoder 사용
    
    * [#5 webMvc and Security] Authentication Provider - in-memory 구현
    
    - Authentication Provider는 메모리 구현인 InMemoryUserDetailsManager
    - 전체 매커니즘이 아직 필요하지 않을 때 빠른 프로토타이핑에 유리
    - user 2명과 admin 정의
    
    * [#5 webMvc and Security] Configuration to Authorize Requests
    
    * [#5 webMvc and Security] Configuration for Form Login
    
    - Form 로그인/로그아웃을 위한 설정

--- src/main/java/kr/co/wmall/wmall_homepage/config/SpringSecurityConfig.java
+++ src/main/java/kr/co/wmall/wmall_homepage/config/SpringSecurityConfig.java
@@ -3,8 +3,18 @@ package kr.co.wmall.wmall_homepage.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,15 +23,67 @@ import java.util.Arrays;
 import java.util.List;
 
 @Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails user1 = User.withUsername("user1")
+                .password(passwordEncoder().encode("user1Pass"))
+                .roles("USERS")
+                .build();
+        UserDetails user2 = User.withUsername("user2")
+                .password(passwordEncoder().encode("user2Pass"))
+                .roles("USERS")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("adminPass"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1,user2,admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/admin/**").hasRole("ADMIN")
+                    .antMatchers("/anonymous*").anonymous()
+                    .antMatchers("/login*","/").permitAll()
+                    .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login.html")
+                    .loginProcessingUrl("/perform_login")
+                    .defaultSuccessUrl("/homepage.html",true)
+                .failureUrl("/login.html?error=true")
+                .failureHandler(authenticationFailureHandler())
+                .and()
+                .logout()
+                .logoutUrl("/perform_logout")
+                .deleteCookies("JSESSIONID")
+                .logoutSuccessHandler(logoutSuccessHandler())
+                .and()
                 .cors().configurationSource(corsConfigurationSource())
         ;
+
         return http.build();
+    }
+
+    @Bean
+    public LogoutSuccessHandler logoutSuccessHandler() {
+        return new SimpleUrlLogoutSuccessHandler();
+    }
+
+    @Bean
+    public AuthenticationFailureHandler authenticationFailureHandler() {
+        return new SimpleUrlAuthenticationFailureHandler();
     }
 
     @Bean
